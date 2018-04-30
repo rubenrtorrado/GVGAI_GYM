20310efd57fc627909cf0c54332f2aa7887765a7
rubenrtorrado <rubentorrado@gmail.com>
2018-04-30 17:12:15 -0400

    pip installation

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 
 setup(name='gym_gvgai',
       version='0.0.1',
-      install_requires=['gym>=0.10.5', 'scipy']
+      install_requires=['gym>=0.10.5', 'scipy', 'tensorflow>=1.6.0', 'numpy>=1.13.3']
       )
