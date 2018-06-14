942dc2b7a60fcff39b3f61acbd1391cc328b5a3e
pbontrager <pbontrager@gmail.com>
2018-06-14 12:16:53 -0400

    added ontology file for new games

---
+++ gym_gvgai/envs/gvgai/src/ontology/effects/unary/KillIfScoreNegative.java
@@ -0,0 +1,37 @@
+package ontology.effects.unary;
+
+import core.vgdl.VGDLSprite;
+import core.content.InteractionContent;
+import core.game.Game;
+import core.logging.Logger;
+import core.logging.Message;
+import ontology.effects.Effect;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 23/10/13
+ * Time: 15:21
+ * This is a Java port from Tom Schaul's VGDL - https://github.com/schaul/py-vgdl
+ */
+public class KillIfScoreNegative extends Effect {
+
+    public KillIfScoreNegative(InteractionContent cnt)
+    {
+        is_kill_effect = true;
+        this.parseParameters(cnt);
+    }
+
+    @Override
+    public void execute(VGDLSprite sprite1, VGDLSprite sprite2, Game game) {
+	if(sprite1 == null){
+	    Logger.getInstance().addMessage(new Message(Message.WARNING, "1st sprite can't be EOS with KillSprite interaction."));
+	    return;
+	}
+
+        //boolean variable set to false to indicate the sprite was not transformed
+		if (game.getScore() < 0.0){
+			game.killSprite(sprite1, false);
+		}
+    }
+}
